2da265f89895f9e1055635016beec9c47cadc06f
heoheon <heoheon.dev@gmail.com>
2020-07-11 19:00:48 +0900

    Arrays 이용하여 소트 후 포함되어 있지 않은 애 비교

--- algorithm/java/question/test01.java
+++ algorithm/java/question/test01.java
@@ -1,11 +1,60 @@
-package algorithm.java.question-1;
+package algorithm.java.question;
+
+
+import java.util.*;
 
 public class test01 {
 
 
+    public static void main(String[] args){
 
 
-    String[] participant = []
+        test01 test = new test01();
+
+        String[] participant = {"heon", "kim", "choi"};
+        String[] completion = {"kim", "choi"};
+
+        String rtn = test.solution(participant, completion);
+
+        System.out.println(rtn);
+
+
+
+    }
+
+
+
+
+    public String solution (String[] participant, String[] completion ){
+
+        Arrays.sort(participant);
+
+        Arrays.sort(completion);
+        
+        
+        int i;
+
+        for (i=0;  i < completion.length; i++){
+
+            if (!participant[i].equals(completion[i])){
+
+                return participant[i];
+            }
+
+
+        }
+
+
+
+
+
+        return null;
+    }
+
+
+
+
+
 
 
 
