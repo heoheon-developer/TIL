e829a058fa5c728c6f87939a1e94435dec5e1067
heon <heoheon.dev@gmail.com>
2020-07-23 16:14:31 +0900

    초기설정

--- hello-spring/src/main/java/hello/hellospring/HelloSpringApplication.java
+++ hello-spring/src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -2,7 +2,8 @@ package hello.hellospring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// 스프링 부트 어플리케이션 실행
+// 내장 톰캣으로 실행
 @SpringBootApplication
 public class HelloSpringApplication {
 
