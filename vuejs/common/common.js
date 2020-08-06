e2684290c1ce08f76a06a78e56af1d786f263aaa
heoheon <heoheon.dev@gmail.com>
2020-08-06 22:32:01 +0900

    common

---
+++ vuejs/common/common.js
@@ -0,0 +1,5 @@
+// admin
+export const baseURL = 'http://localhost:8000';
+//export const baseURL = 'http://scontract.kr/dk';
+export const staticURL = 'http://localhost:8000';
+//export const staticURL = 'http://scontract.kr';
