a98b41fa66863fcc59213f3d8b52b961f1913613
heon <heoheon.dev@gmail.com>
2020-08-21 16:56:24 +0900

    MemberService

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -4,6 +4,7 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public class MemberService {
@@ -30,6 +31,14 @@ public class MemberService {
             throw new IllegalStateException("이미 존재하는 회원입니다.");
 
         });
+    }
+
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
     }
 
 
