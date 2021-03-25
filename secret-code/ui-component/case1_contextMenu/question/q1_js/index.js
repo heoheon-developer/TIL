a1d9d8d3aca8d05cf0c2f34f407739cdb87f56ad
heo.h <heo.h@legaltech.co.kr>
2021-03-26 08:35:20 +0900

    ui-component

---
+++ secret-code/ui-component/case1_contextMenu/question/q1_js/index.js
@@ -0,0 +1,13 @@
+// Write Javascript code here!
+
+
+const items = document.querySelectorAll('.item');
+
+items.forEach(function(item){
+    item.addEventListener('click', function(e){
+        item.classList.toggle('open');
+        items.forEach(function(elem){
+            if (elem !== item) elem.classList.remove('open');
+        });
+    });
+});
